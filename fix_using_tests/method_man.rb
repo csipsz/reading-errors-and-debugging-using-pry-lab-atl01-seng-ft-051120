ca2bc838df1f0cacd83c890aee52d2cf1c01f8ba
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-03 15:44:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,10 @@
  require 'pry'
 
 def start_game(player1, player2)
-  binding.pry
   "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
+  
 end
